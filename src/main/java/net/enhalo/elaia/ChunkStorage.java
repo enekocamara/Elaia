6c0c98c3e4ed9a35e026d997bbd76502e20ff490
enekocamara <eneko.ca.pal@gmail.com>
2025-11-03 11:40:10 +0100

    trying to get panama working

---
+++ src/main/java/net/enhalo/elaia/ChunkStorage.java
@@ -0,0 +1,5 @@
+package net.enhalo.elaia;
+
+public class ChunkStorage {
+
+}
---
+++ src/main/java/net/enhalo/elaia/mixin/ChunkMixin.java
@@ -0,0 +1,21 @@
+package net.enhalo.elaia.mixin;
+
+import net.fabricmc.fabric.api.attachment.v1.AttachmentTarget;
+import net.minecraft.block.Block;
+import net.minecraft.world.StructureHolder;
+import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.chunk.light.LightSourceView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(Chunk.class)
+
+public abstract class ChunkMixin implements BiomeAccess.Storage, LightSourceView, StructureHolder, AttachmentTarget {
+    @Unique
+    private MyBlockStorage elaia$storage; // your custom memory layout
+    Block
+}
+
